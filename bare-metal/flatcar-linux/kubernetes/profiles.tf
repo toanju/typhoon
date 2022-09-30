b986b800d7d96af43e1929dd54228297e79e7853
Tobias Jungel <tobias.jungel@gmail.com>
2022-09-30 22:29:05 +0200

    Add installation of latest uefi firmware
    Firmware zip archive is expected to be located in the matchbox
    assets/uefi folder.
    
    Firmware located here: https://github.com/pftf/RPi4/

--- bare-metal/flatcar-linux/kubernetes/profiles.tf
+++ bare-metal/flatcar-linux/kubernetes/profiles.tf
@@ -53,6 +53,7 @@ data "ct_config" "install" {
     os_channel         = local.channel
     os_version         = var.os_version
     ignition_endpoint  = format("%s/ignition", var.matchbox_http_endpoint)
+    matchbox_endpoint  = var.matchbox_http_endpoint
     mac                = concat(var.controllers.*.mac, var.workers.*.mac)[count.index]
     install_disk       = var.install_disk
     ssh_authorized_key = var.ssh_authorized_key
@@ -69,6 +70,7 @@ data "ct_config" "cached-install" {
     os_channel         = local.channel
     os_version         = var.os_version
     ignition_endpoint  = format("%s/ignition", var.matchbox_http_endpoint)
+    matchbox_endpoint  = var.matchbox_http_endpoint
     mac                = concat(var.controllers.*.mac, var.workers.*.mac)[count.index]
     install_disk       = var.install_disk
     ssh_authorized_key = var.ssh_authorized_key
